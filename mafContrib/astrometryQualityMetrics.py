ab1e80b0964d23454c2ed690b6a278eee37e066c
Peter Yoachim <yoachim@uw.edu>
2015-11-20 18:25:37 -0700

    started updating astrometry metrics

--- mafContrib/astrometryQualityMetrics.py
+++ mafContrib/astrometryQualityMetrics.py
@@ -1,6 +1,7 @@
 import numpy as np
 from lsst.sims.maf.metrics import BaseMetric
 from scipy.stats import spearmanr
+from lsst.sims.maf.utils.astrometryUtils import m52snr, astrom_precision
 
 __all__ =['ParallaxCoverageMetric','ParallaxHADegenMetric']
 
@@ -8,37 +9,71 @@ class ParallaxCoverageMetric(BaseMetric):
     """
     Check how well the parallax factor is distributed.
     """
-    def __init__(self, metricName='ParallaxCoverageMetric', **kwargs):
-        cols = ['ra_pi_amp', 'dec_pi_amp']
+    def __init__(self, metricName='ParallaxCoverageMetric', m5Col='fiveSigmaDepth',
+                 mjdCol='expMJD', filterCol='filter', seeingCol='finSeeing',
+                 rmag=20., SedTemplate='flat', badval=-666,
+                 atm_err=0.01,**kwargs):
+
+        cols = ['ra_pi_amp', 'dec_pi_amp', m5Col, mjdCol, filterCol, seeingCol]
         units = 'ratio'
         super(ParallaxCoverageMetric, self).__init__(cols,
                                                      metricName=metricName, units=units,
                                                       **kwargs)
+        self.m5Col = m5Col
+        self.seeingCol = seeingCol
+        self.filterCol = filterCol
+        self.mjdCol = mjdCol
 
-    def _aveFactor(self, ra_pi_amp, dec_pi_amp):
+        filters=['u','g','r','i','z','y']
+        self.mags={}
+        if SedTemplate == 'flat':
+            for f in filters:
+                self.mags[f] = rmag
+        else:
+            self.mags = utils.stellarMags(SedTemplate, rmag=rmag)
+        self.atm_err = atm_err
+
+
+    def _aveFactor(self, ra_pi_amp, dec_pi_amp, weights):
         """
         Find the average parallax factor:
         1 is good, 0 is bad
         """
-        return np.mean(np.sqrt(ra_pi_amp**2 + dec_pi_amp**2 ))
+        radius = np.sqrt(ra_pi_amp**2 + dec_pi_amp**2 )
+        # weights are for ra or dec, so the quad sum means...
+        # XXX
+        return np.average(radius, weights=weights)
 
-    def _avePosition(self, ra_pi_amp, dec_pi_amp):
+    def _avePosition(self, ra_pi_amp, dec_pi_amp, weights):
         """
         Find the average parallax factor.
         0 is good
         1 is bad
         """
-        ave_ra = np.mean(ra_pi_amp)
-        ave_dec = np.mean(dec_pi_amp)
+        ave_ra, sumOfWeights = np.average(ra_pi_amp, weights=weights)
+        ave_dec, sumOfWeights = np.average(dec_pi_amp, weights=weights)
         ave_r = np.sqrt( ave_ra**2 + ave_dec**2)
 
         return ave_r
+
+    def computeWeights(self, dataSlice):
+        filters = np.unique(dataSlice[self.filterCol])
+        snr = np.zeros(len(dataSlice), dtype='float')
+        # compute SNR for all observations
+        for filt in filters:
+            good = np.where(dataSlice[self.filterCol] == filt)
+            snr[good] = m52snr(self.mags[filt], dataSlice[self.m5Col][good])
+        # Compute centroid uncertainty in each visit
+        position_errors = np.sqrt(astrom_precision(dataSlice[self.seeingCol], snr)**2+self.atm_err**2)
+        weights = 1./position_errors**2
+        return weights
 
     def run(self, dataSlice, slicePoint=None):
 
         # XXX-Need to add a SNR cut or something, so only include points that are relevant
 
-        aveFac = self._aveFactor(dataSlice['ra_pi_amp'], dataSlice['dec_pi_amp'])
+        weights = self.computeWeights(dataSlice)
+        aveFac = self._aveFactor(dataSlice['ra_pi_amp'], dataSlice['dec_pi_amp'], weights)
         avePos = self._avePosition(dataSlice['ra_pi_amp'], dataSlice['dec_pi_amp'])
 
         result = (1-avePos)*aveFac
@@ -57,7 +92,8 @@ class ParallaxHADegenMetric(BaseMetric):
         super(ParallaxHADegenMetric, self).__init__(cols,
                                                     metricName=metricName,
                                                     units=units, **kwargs)
-        # I can take a dot product of parallax displacement and the DCR displacement.
+
+        # XXX--need to update to include a SNR limit.
 
     def run(self, dataSlice, slicePoint=None):
 
